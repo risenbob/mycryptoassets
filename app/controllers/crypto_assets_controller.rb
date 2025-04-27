76cf5e91017073798e9fcea0e1bef8c9f8173846
briu <briu@tut.by>
2025-04-27 18:45:36 +0300

    create crypto assets controller

---
+++ app/controllers/crypto_assets_controller.rb
@@ -0,0 +1,26 @@
+class CryptoAssetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def index
+  end
+end
