54c066209f313bc0d08703f74624905a60dbcc55
briu <briu@tut.by>
2025-04-27 17:19:41 +0300

    add cryptocurrency model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_27_133402) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_27_141903) do
+  create_table "crypto_currencies", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
