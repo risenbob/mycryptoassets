0e8307df52f956b020bcc7d0c52187635ceaa95e
briu <briu@tut.by>
2025-04-27 17:21:06 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if CryptoCurrency.count == 0
+  CryptoCurrency.create!(name: 'Bitcoin', code: 'BTC')
+  CryptoCurrency.create!(name: 'Ethereum', code: 'ETH')
+  CryptoCurrency.create!(name: 'Ripple', code: 'XRP')
+  CryptoCurrency.create!(name: 'Litecoin', code: 'LTC')
+end
